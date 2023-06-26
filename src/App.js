12d53cc82a724f381dd18515461a24a9c8e14573
S M Simon <koushiksimon@gmail.com>
2023-06-26 21:51:07 +0600

    first development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Quiz from './Quiz';
+import { jsQuizz } from './assets/constants';
+import { resultInitialState } from './assets/constants';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Quiz questions = {jsQuizz.questions}/>
     </div>
   );
 }
